421e14078f5197aa9e396acdebc1b3a07efc60d0
Noor757 <122727687+Noor757@users.noreply.github.com>
2024-01-05 20:07:59 +0500

    Second Commit

---
+++ FoodApplication/Controllers/RecipeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FoodApplication.Models;
+namespace FoodApplication.Controllers
+{
+    public class RecipeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult GetRecipeCard([FromBody] List<Recipe> recipes)
+        {
+            return PartialView("_RecipeCard", recipes);
+        }
+    }
+}
--- FoodApplication/wwwroot/js/site.js
+++ FoodApplication/wwwroot/js/site.js
@@ -1,4 +1,21 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿
+let apiURL = "https://forkify-api.herokuapp.com/api/v2/recipes";
+let apikey = "e763bea8-2351-4aa6-a6f1-5f0613b9f82f";
+async function GetRecipes(recipeName,id,isAllShow) {
+    let resp = await fetch(`${apiURL}?search=${recipeName}&key=${apikey}`);
+    let result = await resp.json();
+    let Recipes = isAllShow ? result.data.recipes : result.data.recipes.slice(1, 7);
+    showRecipes(Recipes, id);
+}
+function showRecipes(recipes, id) {
+    $.ajax({
+        contentType: "application/json; charset=utf-8",
+        dataType: 'html',
+        type: 'POST',
+        url: '/Recipe/GetRecipeCard',
+        data: JSON.stringify(recipes),
+        success: function (htmlResult) {
+            $('#' + id).html(htmlResult);
+        }
+    });
+}
